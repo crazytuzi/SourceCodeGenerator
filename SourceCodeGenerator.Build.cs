fcc07fcdbe73fb47d0dd7db4767170166b28a42e
Srkmn <47887032@qq.com>
2024-11-17 14:09:14 +0800

    Source Code Generator

---
+++ SourceCodeGenerator.Build.cs
@@ -0,0 +1,40 @@
+﻿using UnrealBuildTool;
+
+namespace UnrealBuildTool.Rules
+{
+	public class SourceCodeGenerator : ModuleRules
+	{
+		public SourceCodeGenerator(ReadOnlyTargetRules Target) : base(Target)
+		{
+			bEnforceIWYU = false;
+
+			PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+			PublicIncludePaths.AddRange(
+				new string[]
+				{
+					"Programs/UnrealHeaderTool/Public",
+				}
+			);
+
+
+			PrivateIncludePaths.AddRange(
+				new string[]
+				{
+					"SourceCodeGenerator/Private",
+				}
+			);
+
+
+			PrivateDependencyModuleNames.AddRange(
+				new string[]
+				{
+					"Core",
+					"CoreUObject",
+				}
+			);
+
+			PublicDefinitions.Add("HACK_HEADER_GENERATOR=1");
+		}
+	}
+}
