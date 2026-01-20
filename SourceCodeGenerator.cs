daf836b250fe9f572f768d5e47c1ce93b650ecb9
yawn831 <yawn831831@gmail.com>
2026-01-20 14:41:59 +0800

    feat: 头文件注释

--- SourceCodeGenerator.cs
+++ SourceCodeGenerator.cs
@@ -63,6 +63,13 @@ namespace SourceCodeGeneratorUbtPlugin
         private const string HeaderSuffix = ".header.inl";
 
         private readonly HashSet<string> ClassBlacklist = new();
+        
+        private const string GeneratedHeaderComment = 
+            "/*===========================================================================\n" +
+            "    Generated code exported from UnrealCSharp SourceCodeGenerator.\n" +
+            "    DO NOT modify this manually!\n" +
+            "===========================================================================*/\n" +
+            "\n";
 
         public SourceCodeGenerator(IUhtExportFactory factory)
         {
@@ -381,6 +388,8 @@ namespace SourceCodeGeneratorUbtPlugin
 
         private bool ExportClass(StringBuilder builder, UhtClass classObj)
         {
+            builder.Append(GeneratedHeaderComment);
+            
             builder.Append("#pragma once\r\n\r\n" +
                            "PRAGMA_DISABLE_DEPRECATION_WARNINGS\r\n\r\n");
 
